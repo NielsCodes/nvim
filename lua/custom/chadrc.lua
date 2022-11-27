5e58f46385efe51fdd29ba6e18c35c7621080c06
Niels Kersic <hi@niels.codes>
2022-11-27 22:34:07 +0100

    highlight yanked

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,4 +1,11 @@
 -- First read our docs (completely) then check the example_config repo
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd[[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+  augroup END
+]]
 
 local M = {}
 
