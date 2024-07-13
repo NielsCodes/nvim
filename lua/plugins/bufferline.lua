7cfd2f4b34902f0caf9bc38a13eb54bef3c0b3bd
Niels Kersic <niels@zzapps.nl>
2024-07-13 21:16:44 +0200

    feat: add bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.opt.termguicolors = true
+		local bufferline = require("bufferline")
+		bufferline.setup({})
+		vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>", {})
+		vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>", {})
+	end,
+}
