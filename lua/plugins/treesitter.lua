26f6b6a9fde6b79690a97f21bda10e4ea35797e8
Niels Kersic <niels@zzapps.nl>
2024-07-13 19:41:40 +0200

    chore: ensure treesitter installs svelte

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "go", "javascript", "lua", "markdown" },
+			ensure_installed = { "go", "javascript", "svelte", "lua", "markdown" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
