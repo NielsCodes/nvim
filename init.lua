8511d77d2745456b418a1541fd3e5230b7a7935b
Niels Kersic <niels@zzapps.nl>
2024-07-13 18:38:46 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,20 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Escape insert mode
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, nowait = true })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, nowait = true })
+
+vim.keymap.set("i", "<C-h>", "<Left>", {})
+vim.keymap.set("i", "<C-l>", "<Right>", {})
+vim.keymap.set("i", "<C-j>", "<Down>", {})
+vim.keymap.set("i", "<C-k>", "<Up>", {})
+
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+require("config.lazy")
