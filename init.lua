c9fe38a9099484a33e87b98c27fc889a36b622c5
Niels Kersic <niels@zzapps.nl>
2024-07-13 18:44:05 +0200

    chore: enable relative line numbers

--- init.lua
+++ init.lua
@@ -7,14 +7,19 @@ vim.cmd("set shiftwidth=2")
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, nowait = true })
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, nowait = true })
 
+-- Tmux navigation in insert mode
 vim.keymap.set("i", "<C-h>", "<Left>", {})
 vim.keymap.set("i", "<C-l>", "<Right>", {})
 vim.keymap.set("i", "<C-j>", "<Down>", {})
 vim.keymap.set("i", "<C-k>", "<Up>", {})
 
+-- Tmux navigation in normal mode
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 require("config.lazy")
