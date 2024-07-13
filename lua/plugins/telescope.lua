582c5dd9ae4a266276bf80cc494d85832cc0589d
Niels Kersic <niels@zzapps.nl>
2024-07-13 18:48:32 +0200

    chore: update telescope layout

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
 			vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
@@ -16,6 +17,12 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
+        defaults = {
+          layout_config = {
+            prompt_position = "top"
+          },
+          sorting_strategy = "ascending"
+        },
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
