b32ab60328f2596e4b098d612a9925e527e32da8
Niels Kersic <niels@zzapps.nl>
2024-07-13 20:42:32 +0200

    chore: update neotree to open at current file

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,7 +7,7 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal toggle<CR>", {})
 		vim.keymap.set("n", "<leader>e", ":Neotree focus<CR>", {})
 	end,
 }
