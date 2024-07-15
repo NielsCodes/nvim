f20d942bf6926a97e84d1091b787a683a1042ed9
Niels Kersic <niels@zzapps.nl>
2024-07-15 13:19:27 +0200

    chore: disable bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,12 +1,14 @@
-return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		vim.opt.termguicolors = true
-		local bufferline = require("bufferline")
-		bufferline.setup({})
-		vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>", {})
-		vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>", {})
-	end,
-}
+-- return {
+-- 	"akinsho/bufferline.nvim",
+-- 	version = "*",
+-- 	dependencies = "nvim-tree/nvim-web-devicons",
+-- 	config = function()
+-- 		vim.opt.termguicolors = true
+-- 		local bufferline = require("bufferline")
+-- 		bufferline.setup({})
+-- 		vim.keymap.set("n", "<TAB>", "<cmd>BufferLineCycleNext<CR>", {})
+-- 		vim.keymap.set("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<CR>", {})
+-- 	end,
+-- }
+
+return {}
