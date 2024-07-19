1486ba0262159fab2625d193666c9d36dfe20727
Niels Kersic <niels@zzapps.nl>
2024-07-19 13:51:39 +0200

    feat: add keymap to switch between light and dark themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,15 @@
 return {
-  "shatur/neovim-ayu",
-  config = function()
-    local ayu = require("ayu")
-    ayu.setup({
-      mirage = false,
-      terminal = true,
-      overrides = {}
-    })
-    ayu.colorscheme()
-  end
+	"shatur/neovim-ayu",
+	config = function()
+		local ayu = require("ayu")
+		ayu.setup({
+			mirage = false,
+			terminal = true,
+			overrides = {},
+		})
+		ayu.colorscheme()
+
+		vim.keymap.set("n", "<leader>tl", ":colorscheme ayu-light<CR>", { silent = true })
+		vim.keymap.set("n", "<leader>td", ":colorscheme ayu-dark<CR>", { silent = true })
+	end,
 }
