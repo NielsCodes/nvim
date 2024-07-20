0598839d17193f24ac9c3e3dc5abc2fe4a785cdf
Niels Kersic <niels@zzapps.nl>
2024-07-20 21:17:50 +0200

    chore: update treesitter pre-installed list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,8 +4,21 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "go", "javascript", "svelte", "lua", "markdown", "yaml", "css", "sql", "prisma" },
-			highlight = { enable = true },
+			ensure_installed = {
+				"go",
+				"typescript",
+				"javascript",
+				"svelte",
+				"lua",
+				"markdown",
+				"yaml",
+				"css",
+				"sql",
+				"prisma",
+			},
+			highlight = {
+				enable = true,
+			},
 			indent = { enable = true },
 		})
 	end,
