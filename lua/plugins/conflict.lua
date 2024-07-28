ab3337daf96de902dcf393dde9ed40520b68bd83
Niels Kersic <niels@zzapps.nl>
2024-07-28 19:45:03 +0200

    chore: configure highlight colors for git conflict

--- lua/plugins/conflict.lua
+++ lua/plugins/conflict.lua
@@ -1,6 +1,27 @@
 return {
 	"akinsho/git-conflict.nvim",
 	version = "*",
-	config = true,
-	event = "VeryLazy",
+	config = function()
+		vim.api.nvim_set_hl(0, "GitConflictCurrent", { bg = "#2e5049", fg = "NONE" })
+		vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#344f69", fg = "NONE" })
+
+		require("git-conflict").setup({
+			default_mappings = true,
+			default_commands = true,
+			disable_diagnostics = false,
+			highlights = {
+				incoming = "GitConflictIncoming",
+				current = "GitConflictCurrent",
+			},
+		})
+
+		-- Ensure highlights persist after colorscheme changes
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "*",
+			callback = function()
+				vim.api.nvim_set_hl(0, "GitConflictCurrent", { bg = "#2e5049", fg = "NONE" })
+				vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#344f69", fg = "NONE" })
+			end,
+		})
+	end,
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,11 +5,21 @@ return {
 		ayu.setup({
 			mirage = false,
 			terminal = true,
-			overrides = {},
+			overrides = {
+				GitConflictCurrent = { bg = "#2e5049", fg = "NONE" },
+				GitConflictIncoming = { bg = "#344f69", fg = "NONE" },
+			},
 		})
 		ayu.colorscheme()
-
-		vim.keymap.set("n", "<leader>tl", ":colorscheme ayu-light<CR>", { silent = true })
-		vim.keymap.set("n", "<leader>td", ":colorscheme ayu-dark<CR>", { silent = true })
+		vim.keymap.set("n", "<leader>tl", function()
+			vim.cmd("colorscheme ayu-light")
+			vim.api.nvim_set_hl(0, "GitConflictCurrent", { bg = "#2e5049", fg = "NONE" })
+			vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#344f69", fg = "NONE" })
+		end, { silent = true })
+		vim.keymap.set("n", "<leader>td", function()
+			vim.cmd("colorscheme ayu-dark")
+			vim.api.nvim_set_hl(0, "GitConflictCurrent", { bg = "#2e5049", fg = "NONE" })
+			vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#344f69", fg = "NONE" })
+		end, { silent = true })
 	end,
 }
