da4f78b8e85cc7d77e44db17700f95026d0b363c
Niels Kersic <niels@zzapps.nl>
2024-07-29 10:51:33 +0200

    style: improve line number readability

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -8,6 +8,7 @@ return {
 			overrides = {
 				GitConflictCurrent = { bg = "#2e5049", fg = "NONE" },
 				GitConflictIncoming = { bg = "#344f69", fg = "NONE" },
+				LineNr = { fg = "#5c6773" }, -- A medium gray that works for both themes
 			},
 		})
 		ayu.colorscheme()
@@ -15,11 +16,13 @@ return {
 			vim.cmd("colorscheme ayu-light")
 			vim.api.nvim_set_hl(0, "GitConflictCurrent", { bg = "#2e5049", fg = "NONE" })
 			vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#344f69", fg = "NONE" })
+			vim.api.nvim_set_hl(0, "LineNr", { fg = "#828c99" }) -- Darker gray for light theme
 		end, { silent = true })
 		vim.keymap.set("n", "<leader>td", function()
 			vim.cmd("colorscheme ayu-dark")
 			vim.api.nvim_set_hl(0, "GitConflictCurrent", { bg = "#2e5049", fg = "NONE" })
 			vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#344f69", fg = "NONE" })
+			vim.api.nvim_set_hl(0, "LineNr", { fg = "#5c6773" }) -- Lighter gray for dark theme
 		end, { silent = true })
 	end,
 }
