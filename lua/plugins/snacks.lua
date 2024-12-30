9e6d3b231cc9c2a02491c3b513ea68a09908b28c
Niels Kersic <niels@zzapps.nl>
2024-12-30 11:12:55 +0100

    feat: enable zen mode for snacks.nvim

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,5 +6,9 @@ return {
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 		},
+		zen = {},
 	},
+	init = function()
+		vim.keymap.set("n", "<leader>z", require("snacks.zen").zen, { desc = "Toggle Zen mode" })
+	end,
 }
