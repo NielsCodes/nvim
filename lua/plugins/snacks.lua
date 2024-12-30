3a893dfb6a725070d9101c48e2602662ce4e6c98
Niels Kersic <niels@zzapps.nl>
2024-12-30 11:39:52 +0100

    feat: add keymap to toggle dimming

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,9 +6,31 @@ return {
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 		},
-		zen = {},
+		zen = {
+			toggles = {
+				dim = false,
+			},
+		},
+		animate = {
+			duration = {
+				step = 20,
+				total = 300,
+			},
+		},
+		dim = {},
 	},
 	init = function()
+		local dim_enabled = false
+
 		vim.keymap.set("n", "<leader>z", require("snacks.zen").zen, { desc = "Toggle Zen mode" })
+		vim.keymap.set("n", "<leader>d", function()
+			if dim_enabled then
+				require("snacks.dim").disable()
+				dim_enabled = false
+			else
+				require("snacks.dim").enable()
+				dim_enabled = true
+			end
+		end, { desc = "Toggle dimming" })
 	end,
 }
