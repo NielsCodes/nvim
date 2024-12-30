5f468aed1f81bb922061fbefa359c57a9c80c845
Niels Kersic <niels@zzapps.nl>
2024-12-30 10:45:55 +0100

    feat: add vim-ufo

---
+++ lua/plugins/vim-ufo.lua
@@ -0,0 +1,38 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = {
+		"kevinhwang91/promise-async",
+	},
+	config = function()
+		vim.o.foldcolumn = "0"
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+		vim.keymap.set("n", "K", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				vim.lsp.buf.hover()
+			end
+		end)
+
+		-- Option 2: LSP (since you likely use native LSP)
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
+
+		local language_servers = require("lspconfig").util.available_servers()
+		for _, ls in ipairs(language_servers) do
+			require("lspconfig")[ls].setup({
+				capabilities = capabilities,
+			})
+		end
+
+		require("ufo").setup()
+	end,
+}
